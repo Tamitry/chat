4d4c0890d535c9ebab0b6ac6bef0832221438f5b
Дмитрий Тарликовский <flagger58@gmail.com>
2021-11-25 10:57:28 +0300

    Entities

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageService } from './message.service';
+import Message from './message.entity';
+import User from './../user/user.entity';
 
 @Module({
-  providers: [MessageService]
+  providers: [MessageService],
+  imports: [
+    TypeOrmModule.forFeature([Message, User])
+  ]
 })
-export class MessageModule {}
+export class MessageModule { }
